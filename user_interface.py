366dbaacb0adbb02f13515e2897afcd83368f09a
Jahnavi Gautam <jahnavigautam009@gmail.com>
2026-03-31 19:45:02 +0530

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -4,7 +4,8 @@ def show_menu():
     print("1. Add Expense")
     print("2. View Expenses")
     print("3. Total Expenses")
-    print("4. Exit")
+    print("4. Analyze Expenses")
+    print("5. Exit")
 
 def get_user_choice():
     """Get user input for menu choice"""
